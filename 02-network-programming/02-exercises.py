7f652ec180ac42adb325cd91cdf92fbfc2818e1d
Andriy Boreyko <4boreyko@gmail.com>
2016-05-06 17:09:02 +0300

    4th exercise solution

---
+++ 02-network-programming/02-exercises.py
@@ -0,0 +1,4 @@
+# 4. Update the TCP (tsTclnt.py) and UDP (tsUclnt.py) clients so that the
+# server name is not hardcoded into the application. Allow the user to specify
+# a hostname and port number, and only use the default values if either or both
+# parameters are misiing.
---
+++ 02-network-programming/tsTclnt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from socket import *
+
+
+HOST = input("Enter a hostname:\n") or 'localhost'
+PORT = int(input("Enter a port number:\n") or 21567)
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSock.send(bytes(data, 'utf-8'))
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+tcpCliSock.close()
