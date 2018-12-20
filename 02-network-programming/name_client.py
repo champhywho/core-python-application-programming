d85ca2d7485b8a0433566d62b935765162a9f55b
champhy_who <champhywho@gmail.com>
2018-12-20 19:54:24 +0800

    Update name_client.py
    change indent

--- 02-network-programming/name_client.py
+++ 02-network-programming/name_client.py
@@ -16,17 +16,17 @@ with socket(AF_INET, SOCK_STREAM) as name_client:
         server_data = name_client.recv(BUFSIZ)
         if not server_data: break
         print('>>>', server_data.decode())
-		HOST,PORT=server_data.decode().strip().split(':')
-		ADDR=(HOST,int(PORT))
-		with socket(AF_INET,SOCK_STREAM) as sub_client:
-			sub_client.connect(ADDR)
-			while True:
-				data=input('> ')
-				if not data:
-					break
-				sub_client.send(data.encode('utf-8'))
-				data=sub_client.recv(BUFSIZ)
-				if not data:
-					break
-				print(data.decode('utf-8'))
-			sub_client.close()
+	HOST,PORT=server_data.decode().strip().split(':')
+	ADDR=(HOST,int(PORT))
+	with socket(AF_INET,SOCK_STREAM) as sub_client:
+		sub_client.connect(ADDR)
+		while True:
+			data=input('> ')
+			if not data:
+				break
+			sub_client.send(data.encode('utf-8'))
+			data=sub_client.recv(BUFSIZ)
+			if not data:
+				break
+			print(data.decode('utf-8'))
+		sub_client.close()
